5ac9b4f401ed4a91762aca0c5fb0b1385cc5e49e
Viktor Holovatiuk <stethem696@gmail.com>
2021-02-02 09:04:28 +0200

    DZ5

--- JS/DZ1.js
+++ JS/DZ1.js
@@ -519,3 +519,47 @@
 // // Почта poly@hotmail.de удалена из рассылки.
 
 // console.log(service.mailingList); // ['mango@mail.com', 'ajax@jmail.net', 'kiwi@mail.uk']
+
+// DZ5
+
+// // 1
+// const parent = {
+//   name: "Stacey",
+//   surname: "Moore",
+//   age: 54,
+//   heritage: "Irish",
+// };
+// // Пиши код ниже этой строки
+
+// const child = Object.create(parent);
+
+// // Пиши код выше этой строки
+// child.name = "Jason";
+// child.age = 27;
+
+// 2
+// const ancestor = {
+//   name: 'Paul',
+//   age: 83,
+//   surname: 'Dawson',
+//   heritage: 'Irish'
+// };
+// // Пиши код ниже этой строки
+
+// const parent = Object.create(ancestor);
+// parent.name = 'Stacey';
+// parent.surname = 'Moore';
+// parent.age = 54;
+
+// const child = Object.create(parent);
+// child.name = 'Jason';
+// child.age = 27;
+
+// Пиши код выше этой строки
+
+// 3
+// function Car(brand, model, price) {
+//   this.brand = brand;
+//   this.model = model;
+//   this.price = price;
+// }
